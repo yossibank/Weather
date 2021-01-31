11e1e39ffff6ab7f93ca0d7710a688ba4daf09e7
yossibank <yossibank@yahoo.co.jp>
2021-01-31 15:38:44 +0900

    Initial Commit

---
+++ Weather-iOSTests/Weather_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Weather_iOSTests.swift
+//  Weather-iOSTests
+//
+//  Created by KAMIYAMA YOSHIHITO on 2021/01/31.
+//
+
+import XCTest
+@testable import Weather_iOS
+
+class Weather_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
