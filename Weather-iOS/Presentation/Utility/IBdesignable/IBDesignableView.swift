e24669d523dba7e6e09c58606e82378473681a9d
yossibank <yossibank@yahoo.co.jp>
2021-02-01 23:32:37 +0900

    修正: BorderBottom IBDesignable作成

--- Weather-iOS/Presentation/Utility/IBdesignable/IBDesignableView.swift
+++ Weather-iOS/Presentation/Utility/IBdesignable/IBDesignableView.swift
@@ -32,3 +32,33 @@ class IBDesignableView: UIView {
         #endif
     }
 }
+
+class IBDesignableBorderBottomView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+
+    func commonInit() {
+        let border = CALayer()
+        border.frame = .init(x: 0, y: self.frame.height - 1.0, width: self.frame.width, height: 1.0)
+        border.backgroundColor = UIColor.gray.cgColor
+        self.layer.addSublayer(border)
+    }
+}
