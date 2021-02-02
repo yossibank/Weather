46c55b6f93e448269c399ff7879aec575810e4d3
yossibank <yossibank@yahoo.co.jp>
2021-02-02 23:36:39 +0900

    修正: 都道府県のAPINameを取得できるように変更

--- Weather-iOS/Presentation/Presentation-Domain/Screen/Home/ViewData/PrefectureViewData.swift
+++ Weather-iOS/Presentation/Presentation-Domain/Screen/Home/ViewData/PrefectureViewData.swift
@@ -1,5 +1,4 @@
-enum Prefecture: Int, CaseIterable {
-    /* 北海道地方 */
+enum Prefecture: String, CaseIterable {
     case hokkaido
     /* 東北地方 */
     case aomori
@@ -55,57 +54,68 @@ enum Prefecture: Int, CaseIterable {
     case kagoshima
     case okinawa
 
-    var id: Int { self.rawValue }
-
     var name: String {
+
         switch self {
+
         case .hokkaido : return "北海道"
-        case .aomori   : return "青森"
-        case .iwate    : return "岩手"
-        case .miyagi   : return "宮城"
-        case .akita    : return "秋田"
-        case .yamagata : return "山形"
-        case .fukushima: return "福島"
-        case .ibaraki  : return "茨城"
-        case .tochigi  : return "栃木"
-        case .gunma    : return "群馬"
-        case .saitama  : return "埼玉"
-        case .chiba    : return "千葉"
-        case .tokyo    : return "東京"
-        case .kanagawa : return "神奈川"
-        case .niigata  : return "新潟"
-        case .toyama   : return "富山"
-        case .ishikawa : return "石川"
-        case .fukui    : return "福井"
-        case .yamanashi: return "山梨"
-        case .nagano   : return "長野"
-        case .gifu     : return "岐阜"
-        case .shizuoka : return "静岡"
-        case .aichi    : return "愛知"
-        case .mie      : return "三重"
-        case .shiga    : return "滋賀"
-        case .kyoto    : return "京都"
-        case .osaka    : return "大阪"
-        case .hyogo    : return "兵庫"
-        case .nara     : return "奈良"
-        case .wakayama : return "和歌山"
-        case .tottori  : return "鳥取"
-        case .shimane  : return "島根"
-        case .okayama  : return "岡山"
-        case .hiroshima: return "広島"
-        case .yamaguchi: return "山口"
-        case .tokushima: return "徳島"
-        case .kagawa   : return "香川"
-        case .ehime    : return "愛媛"
-        case .kochi    : return "高知"
-        case .fukuoka  : return "福岡"
-        case .saga     : return "佐賀"
-        case .nagasaki : return "長崎"
-        case .kumamoto : return "熊本"
-        case .oita     : return "大分"
-        case .miyazaki : return "宮崎"
-        case .kagoshima: return "鹿児島"
-        case .okinawa  : return "沖縄"
+        case .aomori   : return "青森県"
+        case .iwate    : return "岩手県"
+        case .miyagi   : return "宮城県"
+        case .akita    : return "秋田県"
+        case .yamagata : return "山形県"
+        case .fukushima: return "福島県"
+        case .ibaraki  : return "茨城県"
+        case .tochigi  : return "栃木県"
+        case .gunma    : return "群馬県"
+        case .saitama  : return "埼玉県"
+        case .chiba    : return "千葉県"
+        case .tokyo    : return "東京都"
+        case .kanagawa : return "神奈川県"
+        case .niigata  : return "新潟県"
+        case .toyama   : return "富山県"
+        case .ishikawa : return "石川県"
+        case .fukui    : return "福井県"
+        case .yamanashi: return "山梨県"
+        case .nagano   : return "長野県"
+        case .gifu     : return "岐阜県"
+        case .shizuoka : return "静岡県"
+        case .aichi    : return "愛知県"
+        case .mie      : return "三重県"
+        case .shiga    : return "滋賀県"
+        case .kyoto    : return "京都都"
+        case .osaka    : return "大阪府"
+        case .hyogo    : return "兵庫県"
+        case .nara     : return "奈良県"
+        case .wakayama : return "和歌山県"
+        case .tottori  : return "鳥取県"
+        case .shimane  : return "島根県"
+        case .okayama  : return "岡山県"
+        case .hiroshima: return "広島県"
+        case .yamaguchi: return "山口県"
+        case .tokushima: return "徳島県"
+        case .kagawa   : return "香川県"
+        case .ehime    : return "愛媛県"
+        case .kochi    : return "高知県"
+        case .fukuoka  : return "福岡県"
+        case .saga     : return "佐賀県"
+        case .nagasaki : return "長崎県"
+        case .kumamoto : return "熊本県"
+        case .oita     : return "大分県"
+        case .miyazaki : return "宮崎県"
+        case .kagoshima: return "鹿児島県"
+        case .okinawa  : return "沖縄県"
         }
+
+    }
+
+    var apiName: String {
+
+        switch self {
+
+        default:
+            return rawValue.prefix(1).uppercased() + rawValue.dropFirst()
+        }
+
     }
 }
