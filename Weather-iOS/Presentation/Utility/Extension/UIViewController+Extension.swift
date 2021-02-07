a881e2fe6791b713e8b6339b8ef09c9665694761
yossibank <yossibank@yahoo.co.jp>
2021-02-07 15:14:29 +0900

    作成: popOverのためのExtensionクラス

---
+++ Weather-iOS/Presentation/Utility/Extension/UIViewController+Extension.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+extension UIViewController {
+
+    func showPopOver(
+        viewController: UIViewController,
+        sourceView:     UIView,
+        viewSize:       CGSize,
+        direction:      UIPopoverArrowDirection,
+        delegate:       UIPopoverPresentationControllerDelegate?
+    ) {
+        viewController.modalPresentationStyle = .popover
+        viewController.preferredContentSize = viewSize
+
+        let presentationController = viewController.popoverPresentationController
+        presentationController?.sourceView = sourceView
+        presentationController?.sourceRect = sourceView.bounds
+        presentationController?.permittedArrowDirections = direction
+        presentationController?.delegate = delegate
+
+        present(viewController, animated: true)
+    }
+}
