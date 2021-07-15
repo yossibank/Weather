cec4cb6adb15947f53011167bc6b73ea13baf053
yossibank <yossibank@yahoo.co.jp>
2021-07-15 18:16:31 +0900

    develop: プロジェクト名変更

--- WeatherManager-SwiftTests/Training3SwiftTests.swift
+++ WeatherManager-SwiftTests/Training3SwiftTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import Training3_Swift
+@testable import WeatherManager_Swift
 
-class Training3SwiftTests: XCTestCase {
+class WeatherManagerSwiftTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
